1a1ed31f8dc168f227c608c26ab7c9b3ea6ec42a
kovacsmate97 <kovimatyi8@freemail.hu>
2017-02-20 16:35:00 +0100

    Hozzáadva prímkereső

---
+++ Primszamok/PrimeSearcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primszamok
+{
+    class PrimeSearcher
+    {       
+        public static bool primszamlalo(int szam)
+        {
+            for (int i = 1; i <= szam; i++)
+            {
+                if (i == szam)
+                    return true;
+                else
+                {
+                    if (szam % i == 0)
+                        return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
+
